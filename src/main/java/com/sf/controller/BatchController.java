431d563641812c2407fa389034ef9110c7a16562
vivenara <vivenara@LNAR-5CG85045RP.corp.capgemini.com>
2019-04-25 16:51:10 -0500

    xml to json

--- src/main/java/com/sf/controller/BatchController.java
+++ src/main/java/com/sf/controller/BatchController.java
@@ -1,5 +1,12 @@
 package com.sf.controller;
 
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.example.staffschema.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sf.service.BatchService;
 
 import net.sf.json.JSONObject;
@@ -36,5 +45,29 @@ public class BatchController {
 	public JSONObject close(@RequestBody JSONObject staff) {
 		JSONObject output = service.closeBatch(staff);
 		return output;
+	}
+	@RequestMapping(method = RequestMethod.PUT, value = "/batch/xmltojson")
+	@ResponseBody
+	public String xmlToJson(@RequestBody String xmlString) throws JsonProcessingException {
+		JSONObject output = new JSONObject();
+		JAXBContext jaxbContext;
+		String jsonInString="";
+		ObjectMapper mapper = new ObjectMapper();
+		try
+		{
+		    jaxbContext = JAXBContext.newInstance(Root.class);             
+		 
+		    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		 
+		    Root employee = (Root) jaxbUnmarshaller.unmarshal(new StringReader(xmlString));
+		     
+		    jsonInString = mapper.writeValueAsString(employee);
+		    output.put("json", jsonInString);
+		}
+		catch (JAXBException e)
+		{
+		    e.printStackTrace();
+		}
+		return jsonInString;
 	}
 }
